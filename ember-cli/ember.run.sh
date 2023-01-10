25ee81a51221e4a92cc7c678cb99aa96e7082032
vlascik <vlascik@users.noreply.github.com>
2023-01-10 22:24:18 +0100

    Update ember.run.sh

--- ember-cli/ember.run.sh
+++ ember-cli/ember.run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec node --harmony --inspect=0.0.0.0:9229 --expose_debug_as=v8debug /apps/${APP_FOLDER}/node_modules/ember-cli/bin/ember serve
+exec node --harmony --inspect=0.0.0.0:9229 /apps/${APP_FOLDER}/node_modules/ember-cli/bin/ember serve
